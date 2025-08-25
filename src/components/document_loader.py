4acb2629be67a7545ab6223c112709e72ef559b5
Saifansari-ai <saifansari300b@gmail.com>
2025-08-25 10:40:07 +0530

    dvc pipeline setup

--- src/components/document_loader.py
+++ src/components/document_loader.py
@@ -1,18 +1,31 @@
 import PyPDF2
+import sys
+import os
+from src.constant import *
+from src.logger import logging
+from src.exception import MyException
+
+
 def extract_test(pdf_path,txt_path):
-    text = ""
-    with open(pdf_path, 'rb') as f:
-        reader = PyPDF2.PdfReader(f)
-        for page in reader.pages:
-            text += page.extract_text() or ""
-    
-    
-    with open(txt_path, 'w', encoding='utf-8') as f:
-        f.write(text)
+
+    try:
+        logging.info(f"Extracting text from {pdf_path}")
+        text = ""
+        dir_path = os.path.dirname(pdf_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(pdf_path, 'rb') as f:
+            reader = PyPDF2.PdfReader(f)
+            for page in reader.pages:
+                text += page.extract_text() or ""
         
+        logging.info(f"Saving text to {txt_path}")
+        dir_path_1 = os.path.dirname(txt_path)
+        os.makedirs(dir_path_1,exist_ok=True)
+        with open(txt_path, 'w', encoding='utf-8') as f:
+            f.write(text)
+        logging.info(f"✅ Text extracted and saved to {txt_path}")
+    except Exception as e:
+        raise MyException(e, sys) from e
 
-
-pdf_path = ''
-text_path = ''
-extract_test(pdf_path,text_path)
-print(f"Text extracted from {pdf_path} and saved to {text_path}")
+        
+extract_test(PDF_PATH,TEXT_PATH)
---
+++ src/constant/__init__.py
@@ -0,0 +1,3 @@
+# for text extraction
+PDF_PATH = "/home/saif/Desktop/pdf_rag/database/raw_pdf/the-coding-career-handbook.pdf"
+TEXT_PATH = "/home/saif/Desktop/pdf_rag/database/extracted_txt/the-coding-career-handbook.txt"
