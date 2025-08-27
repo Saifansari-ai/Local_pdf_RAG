78d8aafef3078eee0207a35df4e56594c8a95475
Saifansari-ai <saifansari300b@gmail.com>
2025-08-27 06:04:23 +0530

    Embedding generated locally using dvc pipeline

--- src/components/embed_gen.py
+++ src/components/embed_gen.py
@@ -4,52 +4,56 @@ from src.logger import logging
 from src.exception import MyException
 from sentence_transformers import SentenceTransformer
 import os
+from src.constant import *
 
 try:
     logging.info("✅ Generating embeddings locally")
 
-    MODEL_PATH = "/home/saif/Desktop/pre_trained_llms/bge-base-en-v1.5"
-    logging.info(f"Loading embedding model from {MODEL_PATH}")
+    logging.info(f"Loading embedding model from {EMBED_MODEL_PATH}")
 
     # Load model once
-    model = SentenceTransformer(MODEL_PATH)
+    model = SentenceTransformer(EMBED_MODEL_PATH)
     logging.info("✅ Loaded embedding model locally")
 
-    # Load chunks
-    logging.info("Loading chunks")
-    with open("/home/saif/Desktop/pdf_rag/data/chunks/chunks.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-    logging.info("✅ Loaded chunks")
+    for file in os.listdir(CHUNK_FILE):
+        
+        chunk_file = os.path.join(CHUNK_FILE, file)
+        logging.info(f"Loading chunks from {chunk_file}")
+        with open(chunk_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        logging.info(f"✅ Loaded chunks from {chunk_file}")
+        
+        # Extract text
+        logging.info("Extracting text from JSON")
+        texts = [chunk["text"] for chunk in data]
+        logging.info(f"✅ Extracted {len(texts)} text chunks from JSON")
 
-    # Extract text
-    logging.info("Extracting text from JSON")
-    texts = [chunk["text"] for chunk in data]
-    logging.info(f"✅ Extracted {len(texts)} text chunks from JSON")
+        # Generate embeddings in batches
+        logging.info("Generating embeddings locally in batches")
+        embeddings_array = model.encode(
+            texts,
+            batch_size=BATCH_SIZE,          
+            convert_to_numpy=CONVERT_TO_NUMPY,
+            show_progress_bar=SHOW_PROGRESS_BAR  
+        )
 
-    # Generate embeddings in batches
-    logging.info("Generating embeddings locally in batches")
-    embeddings_array = model.encode(
-        texts,
-        batch_size=32,          
-        convert_to_numpy=True,
-        show_progress_bar=True  
-    )
+        # Pair embeddings with texts
+        embeddings = [
+            {"text": text, "embedding": embedding.tolist()}
+            for text, embedding in zip(texts, embeddings_array)
+        ]
 
-    # Pair embeddings with texts
-    embeddings = [
-        {"text": text, "embedding": embedding.tolist()}
-        for text, embedding in zip(texts, embeddings_array)
-    ]
+        logging.info("✅ Generated embeddings locally")
 
-    logging.info("✅ Generated embeddings locally")
+        # Save to JSON
+        logging.info("Saving embeddings to JSON")
+        os.makedirs(EMBEDDINGS_PATH, exist_ok=True)
+        path = os.path.join(EMBEDDINGS_PATH, f"{file}.json")
 
-    # Save to JSON
-    logging.info("Saving embeddings to JSON")
-    os.makedirs("/home/saif/Desktop/pdf_rag/data/embeddings", exist_ok=True)
-    with open("/home/saif/Desktop/pdf_rag/data/embeddings/embedding.json", "w", encoding="utf-8") as f:
-        json.dump(embeddings, f, ensure_ascii=False, indent=2)
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(embeddings, f, ensure_ascii=False, indent=2)
 
-    logging.info("✅ Embeddings saved to embedding.json")
+        logging.info("✅ Embeddings saved to embedding.json")
 
 except Exception as e:
     raise MyException(e, sys) from e
--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -10,3 +10,10 @@ CHUNK_FILE = "/home/saif/Desktop/pdf_rag/database/chunk"
 CHUNK_SIZE = 1000                  
 CHUNK_OVERLAP = 200 
 SOURCE_NAME = "cleaned_text"  
+
+# For embedding
+EMBED_MODEL_PATH = "/home/saif/Desktop/pre_trained_llms/bge-base-en-v1.5"
+BATCH_SIZE=32       
+CONVERT_TO_NUMPY=True
+SHOW_PROGRESS_BAR=True 
+EMBEDDINGS_PATH = "/home/saif/Desktop/pdf_rag/database/embeddings"
